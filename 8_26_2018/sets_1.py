0df0e77045c8ff235a1285832dabce79856e73b2
unknown <yakanna001@gmail.com>
2018-08-29 22:37:53 +0530

    added sets ordered information

--- 8_26_2018/sets_1.py
+++ 8_26_2018/sets_1.py
@@ -5,18 +5,20 @@ a = [10,20,10,30,20,30]
 #sets properties..
 #it wont allow duplicates ..
 #immutable..(sets also as keys for dicts)
-print a
+print (a)
 
 a = set(a)
-print a
+print (a)
 
 a = "this is python"
 a =set(a)
-print a
+print (a)
 
 a = ["this", "is", "this","that"]
 a =set(a)
-print a
+print (a)
+
+# print (a[0]) #sets are not ordered 
 
 
 
