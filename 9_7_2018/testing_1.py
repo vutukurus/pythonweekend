da8c03d05e6ab1aa1620cd8fd1ceee489f19f316
safads <vutukurus@gmail.com>
2018-09-07 20:57:16 +0530

    adfs

---
+++ 9_7_2018/testing_1.py
@@ -0,0 +1,10 @@
+def testing(numbers):
+	b= []
+	c= ""
+	for i in numbers:
+		if type(i) == type(c): #str, isinstance(i,str)
+			return "Number are not allowed"
+		b.append(i+10)
+	return b
+	
+#print testing(["a","b","c"])
